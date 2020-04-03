fcf09dc4e8b3974c14422add6734dbc7dc3d6669
Adrien Rault <adrien@rault.io>
2020-04-03 13:57:58 +0200

    add install softwares

---
+++ InstallSoftwares.ps1
@@ -0,0 +1,24 @@
+Function DownloadFile {
+  param([string]$Url, [string]$Output)
+
+  Import-Module BitsTransfer
+  Start-BitsTransfer -Source $Url -Destination $Output  
+}
+
+Function InstallMSI {
+  param([string]$Path, [string]$Options)
+
+  Start-Process msiexec.exe -Wait -ArgumentList "/I `"$Path`" /quiet $Options"
+}
+
+Function InstallExe {
+  param([string]$Path)
+
+  Start-Process $Path -Wait
+}
+
+DownloadFile "https://download.mozilla.org/?product=firefox-msi-latest-ssl&os=win64&lang=en-US" "firefox.msi"
+InstallMSI "$pwd/firefox.msi" "TASKBAR_SHORTCUT=true DESKTOP_SHORTCUT=false START_MENU_SHORTCUT=true"
+
+DownloadFile "https://discordapp.com/api/download?platform=win" "discord.exe"
+InstallExe "$pwd/discord.exe"
